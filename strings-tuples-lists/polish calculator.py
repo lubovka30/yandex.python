ca519ff025b876a6cad5a8eff2d04dac4825ee78
lubovka30 <kubyshkina2017@inbox.ru>
2023-11-05 19:36:53 +0700

    opn task

--- strings-tuples-lists/polish calculator.py
+++ strings-tuples-lists/polish calculator.py
@@ -9,6 +9,25 @@
 
 result: int = 0
 strInp = input()
+listInp = strInp.split(" ")
 
+while len(listInp) > 1:
+    i: int = 0
+    result = 0
+    if len(listInp) == 1:
+        break
+    while listInp[i].isdigit() or len(listInp[i]) > 1:
+        i += 1
+    else:
+        tmp = listInp[i]
+        if tmp == "+":
+            result = int(listInp[i - 2]) + int(listInp[i - 1])
+        elif tmp == "-":
+            result = int(listInp[i - 2]) - int(listInp[i - 1])
+        elif tmp == "*":
+            result = int(listInp[i - 2]) * int(listInp[i - 1])
+        listInp[i] = str(result)
+        del listInp[i - 2:i]
 
 print(result)
+
